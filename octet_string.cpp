948354e60ef79221949385b9c25978b753485f6a
Tanjeff-N. Moos <tanjeff@not-another-server.net>
2010-11-17 23:39:58 +0100

    Add octet_string::deserialize(), namespace agentx
    Put the octet_string class into the agentx namespace.

--- octet_string.cpp
+++ octet_string.cpp
@@ -1,10 +1,12 @@
 #include "octet_string.h"
 
+using namespace agentx;
+
 data_t octet_string::serialize()
 {
     data_t serialized;
 
-    // encode size
+    // encode size (big endian)
     int size = value.size();
     serialized.push_back(size << 24 & 0xff);
     serialized.push_back(size << 16 & 0xff);
@@ -13,4 +15,26 @@ data_t octet_string::serialize()
 
     // encode value
     serialized += value;
+}
+
+
+void octet_string::deserialize(data_t data, bool big_endian)
+{
+    uint32_t size;
+    if( big_endian )
+    {
+	size =  data[0] << 24;
+	size |= data[1] << 16;
+	size |= data[2] << 8;
+	size |= data[3] << 0;
+    }
+    else
+    {
+	size =  data[0] << 0;
+	size |= data[1] << 8;
+	size |= data[2] << 16;
+	size |= data[3] << 24;
+    }
+
+    value = data.assign(data, 4, data.size()-4);
 }
--- octet_string.h
+++ octet_string.h
@@ -4,19 +4,37 @@
 #include "types.h"
 #include "variable.h"
 
-class octet_string : public variable
+namespace agentx
 {
-    private:
-	data_t value;
-    public:
-	data_t serialize();
-	
-	virtual uint16_t syntax()
-	{
-	    return 4;	// RFC 2741, section 5.4
-	}
-	
-	virtual data_t get() = 0;
-};
+    /**
+     * \brief Represents an Octet String as descibed in RFC 2741, section 5.3
+     */
+    class octet_string : public variable
+    {
+	private:
+	    // We use data_t here although it was not invented for this.
+	    // TODO: Is this a problem?
+	    data_t value;
+
+	public:
+	    /**
+	     * \brief Encode the object as described in RFC 2741, section 5.3
+	     *
+	     * Note:
+	     * We always use big endian.
+	     */
+	    data_t serialize();
+
+
+	    void deserialize(data_t data, bool big_endian=false);
+
+	    virtual uint16_t syntax()
+	    {
+		return 4;	// RFC 2741, section 5.4
+	    }
+
+	    virtual data_t get() = 0;
+    };
+}
 
 #endif

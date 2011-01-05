c39f8bf812d28784f37960d28bd0a4a005e8da24
Tanjeff-N. Moos <tanjeff@not-another-server.net>
2011-01-05 10:11:10 +0100

    Add ClosePDU implementation

---
+++ src/ClosePDU.cpp
@@ -0,0 +1,49 @@
+#include "ClosePDU.h"
+
+using namespace agentx;
+
+ClosePDU::ClosePDU(reason_t _reason) throw(inval_param)
+{
+    // Check parameter
+    if( _reason < 1 || _reason > 6 )
+    {
+	throw(inval_param());
+    }
+
+    // store reason
+    reason = _reason;
+}
+
+
+ClosePDU::ClosePDU(data_t::const_iterator& pos, bool big_endian) throw(parse_error)
+    : PDU(pos, big_endian)
+{
+    // header is parsed by base class constructor
+    // so we continue with reason field:
+    reason = static_cast<reason_t>(*pos++);
+    pos += 3;	// skip reserved fields
+
+    // check whether reason is valid
+    if( reason < 1 || reason > 6)
+    {
+	// invalid
+	throw(parse_error());
+    }
+}
+
+
+data_t ClosePDU::serialize()
+{
+    data_t serialized;
+
+    // Encode reason and reserved fields
+    serialized.push_back(reason);
+    serialized.push_back(0);
+    serialized.push_back(0);
+    serialized.push_back(0);
+
+    // Add Header
+    add_header(2, serialized);
+
+    return serialized;
+}
---
+++ src/ClosePDU.h
@@ -0,0 +1,77 @@
+#ifndef __CLOSEPDU_H__
+#define __CLOSEPDU_H__
+
+#include "exceptions.h"
+#include "types.h"
+#include "PDU.h"
+
+namespace agentx
+{
+    class ClosePDU : public PDU
+    {
+	public:
+	    
+	    enum reason_t
+	    {
+		reasonOther = 1,
+		reasonParseError = 2,
+		reasonProtocolError = 3,
+		reasonTimeouts = 4,
+		reasonShutdown = 5,
+		reasonByManager = 6
+	    };
+	    
+	private:
+	    /**
+	     * \brief The reason why the session is being closed
+	     */
+	    reason_t reason;
+
+	    /**
+	     * \brief Hide default constructor
+	     */
+	    ClosePDU();
+
+	public:
+	    /**
+	     * \brief Constructor
+	     *
+	     * \param reason The reason to session closure. Must be a valid
+	     *               reason.
+	     *
+	     * \exception inval_param If the reason value is invalid.
+	     */
+	    ClosePDU(reason_t reason) throw(inval_param);
+
+	    /**
+	     * \brief Get the reason
+	     */
+	    reason_t get_reason() { return reason; }
+
+	    /**
+	     * \brief Parse constructor
+	     *
+	     * Construct the object by parsing a stream.
+	     *
+	     * \param pos Iterator pointing to the current stream position.
+	     *            The iterator is advanced while reading the header.
+	     *
+	     * \param big_endian Wether the serialized form of the PDU is in
+	     *                   big_endian format.
+	     *
+	     * \exception parse_error If parsing fails, for example because
+	     *			      reading the stream fails or the PDU is 
+	     *			      malformed.
+	     */
+	    ClosePDU(data_t::const_iterator& pos, bool big_endian) throw(parse_error);
+
+
+	    /**
+	     * \brief Serialize the PDU
+	     */
+	    data_t serialize();
+    };
+}
+
+#endif
+

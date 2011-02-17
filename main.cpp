f8ea4f91f1f363a23bfc41e6cbdf44648adfa6b4
Tanjeff-N. Moos <tanjeff@not-another-server.net>
2011-02-17 22:12:25 +0100

    Apply the agentXcpp library license to all files.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011 Tanjeff-Nicolai Moos <tanjeff@cccmz.de>
+ *
+ * This file is part of the agentXcpp library.
+ *
+ * AgentXcpp is free software: you can redistribute it and/or modify
+ * it under the terms of the AgentXcpp library license, version 1, which 
+ * consists of the GNU General Public License and some additional 
+ * permissions.
+ *
+ * AgentXcpp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See the AgentXcpp library license in the LICENSE file of this package 
+ * for more details.
+ */
+
 #include <iostream>
 #include <fstream>
 #include "oid.h"
--- src/PDU.h
+++ src/PDU.h
@@ -1,21 +1,20 @@
 /*
  * Copyright 2011 Tanjeff-Nicolai Moos <tanjeff@cccmz.de>
  *
- * This file is part of agentXcpp.
+ * This file is part of the agentXcpp library.
  *
  * AgentXcpp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the AgentXcpp library license, version 1, which 
+ * consists of the GNU General Public License and some additional 
+ * permissions.
  *
  * AgentXcpp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with AgentXcpp.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * See the AgentXcpp library license in the LICENSE file of this package 
+ * for more details.
  */
 
 #ifndef __PDU_H__

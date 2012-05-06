f7d48d792edce26f295be0e37c26cc8e9a91f2ef
Tanjeff-N. Moos <tanjeff@cccmz.de>
2012-05-06 08:54:44 +0200

    Fix class Octet_String.

--- src/Octet_String.cpp
+++ src/Octet_String.cpp
@@ -20,6 +20,7 @@
 #include <iterator>
 
 #include "Octet_String.hpp"
+#include "helper.hpp"
 
 using namespace agentxcpp;
 
@@ -48,17 +49,13 @@ data_t Octet_String::serialize() const
     data_t serialized;
 
     // encode size (big endian)
-    int size = value.size();
-    serialized.push_back(size >> 24 & 0xff);
-    serialized.push_back(size >> 16 & 0xff);
-    serialized.push_back(size >> 8 & 0xff);
-    serialized.push_back(size >> 0 & 0xff);
+    write32(serialized, value.size());
 
     // encode value
     serialized += value;
 
     // Padding bytes
-    int padsize = 4 - (size % 4);
+    int padsize = 4 - (value.size() % 4);
     if( padsize == 4 ) padsize = 0; // avoid adding 4 padding bytes
     while( padsize-- )
     {
@@ -82,20 +79,7 @@ Octet_String::Octet_String(data_t::const_iterator& pos,
     }
 
     // Get size
-    if( big_endian )
-    {
-	size =  *pos++ << 24;
-	size |= *pos++ << 16;
-	size |= *pos++ << 8;
-	size |= *pos++ << 0;
-    }
-    else
-    {
-	size =  *pos++ << 0;
-	size |= *pos++ << 8;
-	size |= *pos++ << 16;
-	size |= *pos++ << 24;
-    }
+    size = read32(pos, big_endian);
 
     // Octet String emtpy?
     if( size == 0 )

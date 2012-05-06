8dac967a7a6a3b5ab179920ca6b412193e1c3c49
Tanjeff-N. Moos <tanjeff@cccmz.de>
2012-05-06 08:58:09 +0200

    Fix class TimeTicks.cpp

--- src/TimeTicks.cpp
+++ src/TimeTicks.cpp
@@ -16,7 +16,9 @@
  * See the AgentXcpp library license in the LICENSE file of this package 
  * for more details.
  */
+
 #include "TimeTicks.hpp"
+#include "helper.hpp"
 
 using namespace agentxcpp;
 
@@ -25,10 +27,7 @@ data_t TimeTicks::serialize() const
     data_t serialized;
 
     // encode value (big endian)
-    serialized.push_back(value >> 24 & 0xff);
-    serialized.push_back(value >> 16 & 0xff);
-    serialized.push_back(value >> 8 & 0xff);
-    serialized.push_back(value >> 0 & 0xff);
+    write32(serialized, value);
 
     return serialized;
 }
@@ -45,18 +44,5 @@ TimeTicks::TimeTicks(data_t::const_iterator& pos,
     }
 
     // Get value
-    if( big_endian )
-    {
-	value =  *pos++ << 24;
-	value |= *pos++ << 16;
-	value |= *pos++ << 8;
-	value |= *pos++ << 0;
-    }
-    else
-    {
-	value =  *pos++ << 0;
-	value |= *pos++ << 8;
-	value |= *pos++ << 16;
-	value |= *pos++ << 24;
-    }
+    value = read32(pos, big_endian);
 }

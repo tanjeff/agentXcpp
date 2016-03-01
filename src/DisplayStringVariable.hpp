db439c772823ea969860380d0bc6b557715f24a8
Tanjeff-N. Moos <tanjeff@cccmz.de>
2016-03-01 22:39:46 +0100

    Extend copyright notice to 2016.

--- src/DisplayStringVariable.hpp
+++ src/DisplayStringVariable.hpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2011-2016 Tanjeff-Nicolai Moos <tanjeff@cccmz.de>
+ *
+ * This file is part of the agentXcpp library.
+ *
+ * AgentXcpp is free software: you can redistribute it and/or modify
+ * it under the terms of the AgentXcpp library license, version 1, which 
+ * consists of the GNU General Public License and some additional 
+ * permissions.
+ *
+ * AgentXcpp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See the AgentXcpp library license in the LICENSE file of this package 
+ * for more details.
+ */
 #ifndef _DISPLAYSTRING_H_
 #define _DISPLAYSTRING_H_
 
